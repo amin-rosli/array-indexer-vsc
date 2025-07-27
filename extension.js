3e068a2bd2b916e2b1ae7f447e6596b25efa84f1
vzkz <aminrosli@gmail.com>
2025-07-27 18:01:23 +0800

    Rework

--- extension.js
+++ extension.js
@@ -7,40 +7,33 @@ function activate(context) {
 
 	const addIncrementCommand = vscode.commands.registerCommand('array-indexer.addIncrement', function () {
 
-        const editor = vscode.window.activeTextEditor;
-        if (!editor) return;
+		indexer(true);
 
-		const selection = editor.selection;
-        const text = editor.document.getText(selection);
-
-		let result = removeComment(text);
-		result = addComment(result);
-
-		editor.edit(editBuilder => {
-			editBuilder.replace(selection, result);
-		});
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Added index to array!');
 	});
 
 	const removeIncrementCommand = vscode.commands.registerCommand('array-indexer.removeIncrement', function () {
 
-        const editor = vscode.window.activeTextEditor;
-        if (!editor) return;
+		indexer(false);
 
-		const selection = editor.selection;
-        const text = editor.document.getText(selection);
+	});
 
-		let result = removeComment(text);
+	function indexer(addStatus){
+
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) return;
+
+        const selection_text = editor.document.getText(editor.selection);
+
+		let result = removeComment(selection_text);
+
+		if(addStatus == true) {
+			result = addComment(result);
+		}
 
 		editor.edit(editBuilder => {
-			editBuilder.replace(selection, result);
+			editBuilder.replace(editor.selection, result);
 		});
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Removed index from array!');
-	});
+	}
 
 	function removeComment(text){
 		const uncomment_regex = /\/\*i=\d+\s*\*\//g;
@@ -49,60 +42,73 @@ function activate(context) {
 	}
 
 	function addComment(text) {
-	    const comment_regex = setCommentRegex();
-	    let counter = 0;
+	    const comment_char = setCommentChar();
+		const match_regex = new RegExp(`^\\s*[${comment_char}]`);
+		const comment_regex = new RegExp(`[${comment_char}]`);
 
+	    let counter = 0;
 	    let lines = text.split('\n');
 
-	    // First pass: Add raw comments (unformatted)
+	    // First pass: Count max counter
 	    for (let i = 0; i < lines.length; i++) {
-	        // lines[i] = lines[i].replace(comment_regex, `/*i=${counter++}*/$&`);
-			if(comment_regex.test(lines[i])){
+
+			if(match_regex.test(lines[i])){
 				counter++;
 			}
 	    }
 
-	    // Determine the max width for padding
-	    const maxDigits = (counter - 1).toString().length;
-		console.log(maxDigits);
+	    // Determine number of digits of counter
+	    const max_digits = (counter - 1).toString().length;
 		counter = 0;
-	    // Second pass: Format the comments with padding
+
+	    // Second pass: Add comments with padding
 	    for (let i = 0; i < lines.length; i++) {
 
-			let couterPadded =  String(counter).padEnd(maxDigits, ' ');
-			lines[i] = lines[i].replace(comment_regex, `/*i=${couterPadded}*/$&`);
-			counter++;
-	        // const indexComment = lines[i].match(/\/\*i=\d+\*\//);
-
-	        // if (indexComment) {
-	        //     const index = indexComment[0].match(/\d+/);
-
-	        //     const padded = String(index).padEnd(maxDigits, ' ');
-			// 	console.log(padded);
-	        //     lines[i] = lines[i].replace(/\/\*i=\d+\*\//, `/*i=${padded}*/`);
-	        // }
+			if(match_regex.test(lines[i])){
+				let couterPadded =  String(counter).padEnd(max_digits, ' ');
+				lines[i] = lines[i].replace(comment_regex, `/*i=${couterPadded}*/$&`);
+				counter++;
+			}
 	    }
 
+		vscode.window.showInformationMessage(`Indexed "${counter} elements."`);
 	    return lines.join('\n');
 	}
 
-	function setCommentRegex(){
+	function setCommentChar(){
 		const config = vscode.workspace.getConfiguration('array-indexer');
-		const charToIndex = config.get('indexCharacter');
+		const char_index = config.get('indexCharacter');
 
-		if (charToIndex == '{') {
-			return /\{/;
-		} else if (charToIndex == '['){
-			return /\[/;
-		} else if (charToIndex == '['){
-			return /\{/;
-		} else {
-			return /[\{\[\(]/;
-		}
+		const regex_map = {
+			'{': '{',
+			'[': '[',
+			'(': '(',
+			'"': '"',
+			"'": "'",
+			'Alphanumeric': 'a-zA-Z0-9'
+		};
+
+		return regex_map[char_index];
 	}
+
+	const setIndexCharacterCommand = vscode.commands.registerCommand('array-indexer.setIndexCharacter', async function () {
+    	const options = ['{', '[', '(', '"', "'", 'Alphanumeric'];
+
+    	const selected = await vscode.window.showQuickPick(options, {
+    	    placeHolder: 'Select the character to index'
+    	});
+
+    	if (selected) {
+    	    const config = vscode.workspace.getConfiguration('array-indexer');
+    	    await config.update('indexCharacter', selected, vscode.ConfigurationTarget.Global);
+    	    vscode.window.showInformationMessage(`Index character set to "${selected}"`);
+    	}
+	});
 
 	context.subscriptions.push(addIncrementCommand);
 	context.subscriptions.push(removeIncrementCommand);
+	context.subscriptions.push(setIndexCharacterCommand);
+
 }
 
 // This method is called when your extension is deactivated
